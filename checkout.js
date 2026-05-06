542eecc52d1805df60b07f64f22edffd39742c11
aatamanirbhar <95468485+aatamanirbhar@users.noreply.github.com>
2026-05-06 11:02:04 +0000

    lacc

--- checkout.js
+++ checkout.js
@@ -1,9 +1,58 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart =
+  JSON.parse(localStorage.getItem("cart")) || [];
+
+const itemsEl =
+  document.getElementById("checkout-items");
 
 let total = 0;
 
+itemsEl.innerHTML = "";
+
 cart.forEach(item => {
-  total += item.price * item.qty;
+
+  const itemTotal =
+    item.price * item.qty;
+
+  total += itemTotal;
+
+  itemsEl.innerHTML += `
+
+    <div class="checkout-item">
+
+      <img
+        src="${item.image}"
+        class="checkout-item-img"
+      >
+
+      <div class="checkout-item-info">
+
+        <h3>${item.name}</h3>
+
+        ${
+          item.variantName
+          ? `
+            <p class="checkout-variant">
+              ${item.variantName}
+            </p>
+          `
+          : ""
+        }
+
+        <p>
+          Qty: ${item.qty}
+        </p>
+
+        <p class="checkout-item-price">
+          ₹${itemTotal}
+        </p>
+
+      </div>
+
+    </div>
+
+  `;
 });
 
-document.getElementById("checkout-total").innerText = total;
+document.getElementById(
+  "checkout-total"
+).innerText = total;
